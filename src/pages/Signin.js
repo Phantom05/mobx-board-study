b92fe516877f0b71a4f08f88e8ea45e67f80c19f
phantom05 <monster2jy@gmail.com>
2019-10-07 01:29:06 +0900

    사인 인

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -55,7 +55,7 @@ class Signin extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     const loginConfig = {
-      url: '/login',
+      url: '/auth/login',
       data: {
         username: 'test',
         password: 'test'
